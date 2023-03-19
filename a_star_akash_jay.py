e5cac258c01daca56d4a21db0b9b21f86a5691eb
jayprajapati009 <jayprajapati9900@gmail.com>
2023-03-19 17:20:49 -0400

    Boilerplate Upload

---
+++ a_star_akash_jay.py
@@ -0,0 +1,26 @@
+"""
+# Title:    Project-3 : A* Algorithm Implementation
+# Course:   ENPM661 - Planning for Autonomous Robots
+"""
+
+import time
+import heapq
+import cv2
+import numpy as np
+
+__author__ = "Jay Prajapati, Akash Parmar"
+__copyright__ = "Copyright 2023, Project-3"
+__credits__ = ["Jay Prajapati", "Akash Parmar"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__email__ = "jayp@umd.edu", "akasparm@umd.edu"
+
+
+def main():
+    """main function
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
