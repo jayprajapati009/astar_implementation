264ad7b6dea32f6724fc1f1e2888cb7e2d5819b7
jayprajapati009 <jayprajapati9900@gmail.com>
2023-03-19 17:22:38 -0400

    Class update for the datastructure to store nodes

--- a_star_akash_jay.py
+++ a_star_akash_jay.py
@@ -16,6 +16,38 @@ __version__ = "1.0.1"
 __email__ = "jayp@umd.edu", "akasparm@umd.edu"
 
 
+class NODE:
+    """class for data structure to store the node information
+    """
+
+    def __init__(self, coordinates, cost, preced=None, cost_goal=0):
+        """Constructor
+
+        Args:
+            coordinates (list): 
+                [x, y, theta] for the node
+            cost (float): 
+                cost of the node
+            preced (<class 'NODE'>, optional): 
+                parent node for the node. Defaults to None.
+            cost_goal (int, optional): 
+                Cost to reach the goal for the current node. Defaults to 0.
+        """
+        self.coordinates = coordinates
+        self.x_cord = coordinates[0]
+        self.y_cord = coordinates[1]
+        self.theta = coordinates[2]
+        self.cost = cost
+        self.preced = preced
+        self.cost_goal = cost_goal
+
+    def __lt__(self, other):
+        """Defining the comparision operator
+        """
+        comparision = self.cost + self.cost_goal < other.cost + other.cost_goal
+        return comparision
+
+
 def main():
     """main function
     """
